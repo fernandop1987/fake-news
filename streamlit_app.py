1522e892758fefea67cbe2330644f876a2a784a3
fernandop1987 <fernando.pedroarena@gmail.com>
2024-09-10 18:17:23 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,76 @@
 import streamlit as st
-import spacy
-import joblib
+import numpy as np
+import pandas as pd
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from nltk.stem.porter import PorterStemmer
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+
+#######################
+# Load data
+
+# Cargar el archivo CSV usando pandas
+data = pd.read_excel('dataset.xlsx')
+
+#######################
+# Genero el modelo
+
+# merging the author name and news title
+data['contenido'] = data['Fuente']+' '+data['Titular']
+
+# Separamos la variable dependiente e independientes
+
+## Variables independientes
+X = data.drop(columns='Etiqueta', axis=1)
+
+## Variable independiente
+Y = data['Etiqueta']
+
+# Stemming
+port_stem = PorterStemmer()
+def stemming(content):
+    review = re.sub('[^a-zA-Z]',' ',content)
+    review = review.lower()
+    review = review.split()
+    review = [port_stem.stem(word) for word in review if not word in stopwords.words('spanish')]
+    review = ' '.join(review)
+    return review
+
+data['contenido'] = data['contenido'].apply(stemming)
+
+# Separamos data y etiqueta
+
+X = data['contenido'].values
+Y = data['Etiqueta'].values
+
+
+# TF-IDF
+
+# Convertimos el texto a variables numéricas
+vectorizer = TfidfVectorizer()
+vectorizer.fit(X)
+X = vectorizer.transform(X)
+
+#Split Train - Test
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, stratify=Y, random_state=2)
+
+#Regresión Logística
+model = LogisticRegression()
+model.fit(X_train, Y_train)
+
+#######################
 
 # Cargar el modelo y el vectorizador guardados
-model = joblib.load('logistic_regression_model.pkl')
-vectorizer = joblib.load('tfidf_vectorizer.pkl')
+##model = joblib.load('logistic_regression_model.pkl')
+##vectorizer = joblib.load('tfidf_vectorizer.pkl')
 
-# Cargar el lematizador en español de Spacy
-nlp = spacy.load('es_core_news_sm')
-
-# Función de lematización
-def lemmatization(content):
-    doc = nlp(content)
-    lemmas = [token.lemma_ for token in doc if not token.is_stop]
-    return ' '.join(lemmas)
 
 # Función para predecir si una noticia es fake o no
 def predict_fake_news(source, headline):
@@ -21,7 +78,7 @@ def predict_fake_news(source, headline):
     content = source + ' ' + headline
     
     # Preprocesar el contenido
-    preprocessed_content = lemmatization(content)
+    preprocessed_content = stemming(content)
     
     # Transformar el texto con el vectorizador TF-IDF
     content_vectorized = vectorizer.transform([preprocessed_content])
