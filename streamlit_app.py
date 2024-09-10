a6be778cfcfb6d77e6c1293da4c486bcf8718f96
fernandop1987 <fernando.pedroarena@gmail.com>
2024-09-10 17:30:51 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,51 @@
 import streamlit as st
-import pandas as pd
-import math
-from pathlib import Path
+import spacy
+import joblib
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
+# Cargar el modelo y el vectorizador guardados
+model = joblib.load('logistic_regression_model.pkl')
+vectorizer = joblib.load('tfidf_vectorizer.pkl')
 
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
+# Cargar el lematizador en español de Spacy
+nlp = spacy.load('es_core_news_sm')
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
+# Función de lematización
+def lemmatization(content):
+    doc = nlp(content)
+    lemmas = [token.lemma_ for token in doc if not token.is_stop]
+    return ' '.join(lemmas)
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
+# Función para predecir si una noticia es fake o no
+def predict_fake_news(source, headline):
+    # Combinar fuente y titular
+    content = source + ' ' + headline
+    
+    # Preprocesar el contenido
+    preprocessed_content = lemmatization(content)
+    
+    # Transformar el texto con el vectorizador TF-IDF
+    content_vectorized = vectorizer.transform([preprocessed_content])
+    
+    # Hacer la predicción
+    prediction = model.predict(content_vectorized)
+    
+    return prediction[0]
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+# Configurar la aplicación en Streamlit
+st.title('Detección de Fake News')
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
+# Crear entradas de texto para la fuente y el titular
+source = st.text_input('Fuente de la noticia')
+headline = st.text_input('Titular de la noticia')
 
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
-
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
-
-gdp_df = get_gdp_data()
-
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
-
-
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
-
-st.header(f'GDP in {to_year}', divider='gray')
-
-''
-
-cols = st.columns(4)
-
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
+# Botón para hacer la predicción
+if st.button('Verificar'):
+    if source and headline:
+        prediction = predict_fake_news(source, headline)
+        
+        if prediction == 1:
+            st.success('La noticia es verdadera.')
         else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+            st.error('La noticia es falsa.')
+    else:
+        st.warning('Por favor, ingrese tanto la fuente como el titular.')
